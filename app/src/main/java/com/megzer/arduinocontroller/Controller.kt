69593d6d9f20ac870e3e3d80bd9c9b6b9aa1f437
Ahmed Magdi <ammhaa2012@gmail.com>
2022-07-18 22:05:29 +0200

    Changed joystick background

--- app/src/main/java/com/megzer/arduinocontroller/Controller.kt
+++ app/src/main/java/com/megzer/arduinocontroller/Controller.kt
@@ -3,18 +3,39 @@ package com.megzer.arduinocontroller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+import androidx.core.text.HtmlCompat
 
 class Controller : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.controller)
         Controls.hideSystemBars(window)
+
+        //Back button
         findViewById<ImageButton>(R.id.backToMainMenu).setOnClickListener {
             val intent = Intent(this, Main::class.java)
             startActivity(intent)
             finish()
         }
+
+        //Movement info button
+        val movementInfoBtn = findViewById<ImageButton>(R.id.movementInfo)
+        movementInfoBtn.setOnClickListener {
+            val msgHtml = "Moving North East will send :<br>" +
+                    "<font color = 'blue'>4 Directions : </font>2 commands : '${Controls.front}' -> '${Controls.right}'<br>" +
+                    "<font color = 'blue'>8 Directions : </font>1 command  : '${Controls.front_right}'<br>" +
+                    "<font color = 'red'>Note : </font> 8 Directions is recommended when using Omni wheels"
+            val msg = HtmlCompat.fromHtml(msgHtml, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            Controls.dialog(this, msg)
+        }
+        Controls.setClickEffect(movementInfoBtn, "#fff")
+
+
     }
+
+
 }
--- app/src/main/java/com/megzer/arduinocontroller/Controls.kt
+++ app/src/main/java/com/megzer/arduinocontroller/Controls.kt
@@ -1,9 +1,17 @@
 package com.megzer.arduinocontroller
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.text.Spanned
+import android.view.MotionEvent
 import android.view.Window
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import java.lang.IllegalArgumentException
 
 class Controls {
     companion object{
@@ -25,11 +33,58 @@ class Controls {
         var arm_down = 'j'
         var arm_right = 'k'
         var arm_left = 'h'
+        var s1 = "Front"
+        var s2 = "Right"
+        var s3 = "Left"
+        var s4 = "Back"
+        var s1value = 0
+        var s2value = 0
+        var s3value = 0
+        var s4value = 0
 
         fun hideSystemBars(window: Window) {
             val windowInsetsController = ViewCompat.getWindowInsetsController(window.decorView) ?: return
             windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        }
+
+        fun dialog(activity: AppCompatActivity, msg:String){
+            val builder = activity.let { AlertDialog.Builder(it) }
+            builder.apply {
+                setTitle("Info")
+                setMessage(msg)
+                setCancelable(true)
+            }
+            builder.create().show()
+        }
+
+        fun dialog(activity: AppCompatActivity, msg:Spanned){
+            val builder = activity.let { AlertDialog.Builder(it) }
+            builder.apply {
+                setTitle("Info")
+                setMessage(msg)
+                setCancelable(true)
+            }
+            builder.create().show()
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        fun setClickEffect(b: ImageButton, color:String = "#004080" ){
+            val parsedColor =
+                try { Color.parseColor(color) }
+                catch (e: IllegalArgumentException){ Color.parseColor("#004080") }
+            val filter = b.colorFilter
+            b.setOnTouchListener { _, motionEvent ->
+                when(motionEvent.action){
+                    MotionEvent.ACTION_DOWN -> {
+                        b.setColorFilter(parsedColor)
+                    }
+                    MotionEvent.ACTION_UP ->{
+                        b.colorFilter = filter
+                    }
+                }
+                false
+            }
         }
     }
 }
